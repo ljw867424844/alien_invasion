03a6464c47d03f3078395f00a5f2651e2ca92ad3
LJW <ljw867424844@gmail.com>
2025-07-30 14:28:45 +0800

    12.3.4

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,19 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        # 设置背景色
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -15,9 +21,16 @@ class AlienInvasion:
             # 侦听键盘和鼠标事件
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    pygame.quit()
                     sys.exit()
+
+            # 每次循环时都重绘屏幕
+            self.screen.fill(self.settings.bg_color)
+            
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+            self.clock.tick(60)
+            
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
     ai = AlienInvasion()
